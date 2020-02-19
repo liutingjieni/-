4330def9789c99a1572d27bd72703a43f32254fd
liutingjieni <644123183@qq.com>
2020-02-20 04:27:17 +0800

    add

--- http_conn.cpp
+++ http_conn.cpp
@@ -5,7 +5,103 @@
 	> Created Time: 2020年02月18日 星期二 00时38分31秒
  ************************************************************************/
 
-#include "http_conn.h"
+//#include "http_conn.h"
+#define HTTPCONNECTION_H
+
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <stdarg.h>
+#include <errno.h>
+
+class http_conn {
+public:
+    static const int FILENAME_LEN = 200;
+    static const int READ_BUFFER_SIZE = 2048;
+    static const int WRITE_BUFFER_SIZE = 1024;
+
+    enum METHOD { GET = 0, POST, HEAD, PUT, DELETE, 
+                 TRACE, OPTIONS, CONNECT, PATCH };
+    enum CHECK_STATE { CHECK_STATE_REQUESTLINE = 0,
+                       CHECK_STATE_HEADER,
+                       CHECK_STATE_CONTENT };
+    enum HTTP_CODE { NO_REQUEST, GET_REQUEST, BAD_REQUEST,
+                     NO_RESOURCE, FORBIDDEN_REQUEST, FILE_REQUEST,
+                     INTERNAL_ERROR, CLOSED_CONNECTION };
+    enum LINE_STATUS { LINE_OK = 0, LINE_BAD, LINE_OPEN };
+
+    http_conn();
+    ~http_conn();
+
+    void init(int sockfd, const sockaddr_in &addr);
+    void close_conn(bool real_close = true);
+    void process();
+    bool read();
+    bool write();
+
+private:
+    void init();
+    HTTP_CODE process_read();
+    bool process_write(HTTP_CODE ret);
+
+    HTTP_CODE parse_request_line(char *);
+    HTTP_CODE parse_headers(char *text);
+    HTTP_CODE parse_content(char *text);
+    HTTP_CODE do_request();
+    char *get_line() { return m_read_buf + m_start_line; }
+    LINE_STATUS parse_line();
+
+    void unmap();
+    bool add_response(const char *format, ...);
+    bool add_content(const char *content);
+    bool add_status_line(int status, const char *title);
+    bool add_headers(int content_length);
+    bool add_content_length(int content_length);
+    bool add_linger();
+    bool add_blank_line();
+
+public:
+    static int m_epollfd;
+    static int m_user_count;
+
+private:
+    int m_sockfd;
+    sockaddr_in m_address;
+
+    char m_read_buf[READ_BUFFER_SIZE];
+    int m_read_idx;
+    int m_checked_idx;
+    int m_start_line;
+    char m_write_buf[WRITE_BUFFER_SIZE];
+    int m_write_idx;
+
+    CHECK_STATE m_check_state;
+    METHOD m_method;
+
+    char m_real_file[FILENAME_LEN];
+    char *m_url;
+    char *m_version;
+    char *m_host;
+    int m_content_length;
+    bool m_linger;
+
+    char *m_file_address;
+    struct stat m_file_stat;
+    struct iovec m_iv[2];
+    int m_iv_count;
+};
 
 const char *ok_200_title = "OK";
 const char *error_400_title = "BAD Request";
@@ -27,7 +123,7 @@ int setnonblocking(int fd)
 
 }
 
-void addfd(int epollfd, int fd)
+void addfd(int epollfd, int fd, bool one_shot)
 {
     struct epoll_event event;
     event.data.fd = fd;
@@ -70,7 +166,7 @@ void http_conn::init(int sockfd, const sockaddr_in &addr)
     m_sockfd = sockfd;
     m_address = addr;
     int reuse = 1;
-    setsockopt(m_sockfd, SOL_SOCKet, SO_REUSEADDR, &reuse, sizeof(reuse));
+    setsockopt(m_sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
     addfd(m_epollfd, sockfd, true);
     m_user_count++;
     
@@ -83,7 +179,7 @@ void http_conn::init()
     m_linger = false;
 
     m_method = GET;
-    m_url = GET;
+    m_url = 0;
     m_version = 0;
     m_content_length = 0;
     m_host = 0;
@@ -133,7 +229,7 @@ bool http_conn::read()
         bytes_read = recv(m_sockfd, m_read_buf + m_read_idx, 
                           READ_BUFFER_SIZE - m_read_idx, 0);
         if (bytes_read == -1) {
-            if (errno == EAGIN || errno == EWOULDBLOCK) 
+            if (errno == EAGAIN || errno == EWOULDBLOCK) 
                 break;
             return false;
         }
@@ -183,30 +279,30 @@ http_conn::HTTP_CODE http_conn::parse_request_line(char *text)
 
 }
 
-http_conn::HTTP_CODE　http_conn::parse_headers(char *text)
+http_conn::HTTP_CODE http_conn::parse_headers(char *text)
 {
     if (text[0] == '\0') {
-        if (m_context_length != 0) {
+        if (m_content_length != 0) {
             m_check_state = CHECK_STATE_CONTENT;
             return NO_REQUEST;
         }
         return GET_REQUEST;
     }
-    else if (strncasecmp(tetx, "Connection:", 11) == 0) {
+    else if (strncasecmp(text, "Connection:", 11) == 0) {
         text += 11;
-        text += strspn(tetx, "\t");
+        text += strspn(text, "\t");
         if (strcasecmp(text, "kepp-alive") == 0) {
             m_linger = true;
         }
     }
     else if (strncasecmp(text, "Content-Length:", 15) == 0) {
         text += 15;
-        text += strspn(tetx, " \t");
+        text += strspn(text, " \t");
         m_content_length = atol(text);
     }
     else if (strncasecmp(text, "Host:", 5) == 0) {
         text += 5;
-        text += strspn(tetx, " \t");
+        text += strspn(text, " \t");
         m_host = text;
     }
     else {
@@ -218,7 +314,7 @@ http_conn::HTTP_CODE　http_conn::parse_headers(char *text)
 http_conn::HTTP_CODE http_conn::parse_content(char *text)
 {
     if (m_read_idx >= (m_content_length += m_checked_idx)) {
-        text[m_context_length] = '\0';
+        text[m_content_length] = '\0';
         return GET_REQUEST;
     }
     return NO_REQUEST;
@@ -226,7 +322,7 @@ http_conn::HTTP_CODE http_conn::parse_content(char *text)
 
 http_conn::HTTP_CODE http_conn::process_read()
 {
-    LINE_STATE line_status = LINE_OK;
+    LINE_STATUS line_status = LINE_OK;
     HTTP_CODE ret = NO_REQUEST;
     char *text = 0;
 
@@ -254,7 +350,7 @@ http_conn::HTTP_CODE http_conn::process_read()
                             return do_request();
                        break;
                    }
-                   cse CHECK_STATE_CONTENT:
+                   case CHECK_STATE_CONTENT:
                    {
                        ret = parse_content(text);
                        if (ret = GET_REQUEST)
@@ -271,9 +367,317 @@ http_conn::HTTP_CODE http_conn::process_read()
 
 http_conn::HTTP_CODE http_conn::do_request()
 {
-    strcpy(m_real_life, doc_root);
+    strcpy(m_real_file, doc_root);
     int len = strlen(doc_root);
-    strncpy(m_real_life)
+    strncpy(m_real_file, m_url, FILENAME_LEN - len -1);
+    if (stat(m_real_file, &m_file_stat) < 0) {
+        return NO_RESOURCE;
+    }
+    if (!(m_file_stat.st_mode & S_IROTH)) {
+        return FORBIDDEN_REQUEST;
+    }
+    if (S_ISDIR(m_file_stat.st_mode)) {
+        return BAD_REQUEST;
+    }
+    int fd = open(m_real_file, O_RDONLY);
+    m_file_address = (char *)mmap(0, m_file_stat.st_size, PROT_READ,
+                                  MAP_PRIVATE, fd, 0);
+    close(fd);
+    return FILE_REQUEST;
+}
+
+void http_conn::unmap()
+{
+    if (m_file_address) {
+        munmap(m_file_address, m_file_stat.st_size);
+        m_file_address = 0;
+    }
+}
+
+bool http_conn::write()
+{
+    int temp = 0;
+    int bytes_have_send = 0;
+    int bytes_to_send = m_write_idx;
+    if (bytes_to_send == 0) {
+        modfd(m_epollfd, m_sockfd, EPOLLIN);
+        init();
+        return true;
+    }
+
+    while(1) {
+        temp = writev(m_sockfd, m_iv, m_iv_count);
+        if (temp <= -1) {
+            if (errno == EAGAIN) {
+                modfd(m_epollfd, m_sockfd, EPOLLOUT);
+                return true;
+            }
+            unmap();
+            return false;
+        }
+
+        bytes_to_send -= temp;
+        bytes_have_send += temp;
+        if (bytes_to_send <= bytes_have_send) {
+            unmap();
+            if (m_linger) {
+                init();
+                modfd(m_epollfd, m_sockfd, EPOLLIN);
+                return true;
+            }
+            else {
+                modfd(m_epollfd, m_sockfd, EPOLLIN);
+                return false;
+            }
+        }
+    }
 }
 
 
+bool http_conn::add_response(const char* format, ...)
+{
+    if (m_write_idx >= WRITE_BUFFER_SIZE) {
+        return false;
+    }
+    va_list arg_list;
+    va_start(arg_list, format);
+    int len = vsnprintf(m_write_buf + m_write_idx, WRITE_BUFFER_SIZE - 1 - m_write_idx,
+                       format, arg_list);
+    if (len >= (WRITE_BUFFER_SIZE - 1 -m_write_idx )) {
+        return false;
+    }
+    m_write_idx += len;
+    va_end(arg_list);
+    return true;
+}
+
+bool http_conn::add_status_line(int status, const char *title)
+{
+    return add_response("%s %d %s\r\n", "HTTP/1.1", status, title);
+}
+
+bool http_conn::add_headers(int content_len)
+{
+    add_content_length(content_len);
+    add_linger();
+    add_blank_line();
+}
+
+bool http_conn::add_content_length(int content_len)
+{
+    return add_response("Content-length: %d\r\n", content_len);
+}
+
+bool http_conn::add_linger()
+{
+    return add_response("Connection: %s\r\n", ((m_linger == true) ? "keep-alive" : "close"));
+}
+
+bool http_conn::add_blank_line()
+{
+    return add_response("%s", "\r\n");
+}
+
+bool http_conn::add_content(const char *content)
+{
+    return add_response("%s", content);
+}
+
+bool http_conn::process_write(HTTP_CODE ret)
+{
+    switch(ret)
+    {
+        case INTERNAL_ERROR:
+        {
+            add_status_line(500, error_500_title);
+            add_headers(strlen(error_500_form));
+            if (!add_content(error_500_form)) {
+                return false;
+            }
+            break;
+        }
+        case BAD_REQUEST:
+        {
+            add_status_line(400, error_400_title);
+            add_headers(strlen(error_400_form));
+            if (!add_content(error_400_form)) {
+                return false;
+            }
+            break;
+        }
+        case NO_RESOURCE:
+        {
+            add_status_line(404, error_404_title);
+            add_headers(strlen(error_404_form));
+            if (!add_content(error_404_form)) {
+                return false;
+            }
+            break;
+        }
+        case FORBIDDEN_REQUEST:
+        {
+            add_status_line(403, error_403_title);
+            add_headers(strlen(error_403_form));
+            if (!add_content(error_403_form)) {
+                return false;
+            }
+            break;
+
+        }
+        case FILE_REQUEST:
+        {
+            add_status_line(200, ok_200_title);
+            if (m_file_stat.st_size != 0) {
+                add_headers(m_file_stat.st_size);
+                m_iv[0].iov_base = m_write_buf;
+                m_iv[0].iov_len = m_write_idx;
+                m_iv[1].iov_base = m_file_address;
+                m_iv[1].iov_len = m_file_stat.st_size;
+                m_iv_count = 2;
+                return true;
+            }
+            else {
+                const char *ok_string = "<html><body></body></html>";
+                add_headers(strlen(ok_string));
+                if (!add_content(ok_string)) {
+                    return false;
+                }
+            }
+        }
+        defult:
+        {
+            return false;
+        }
+    }
+    m_iv[0].iov_base = m_write_buf;
+    m_iv[0].iov_len = m_write_idx;
+    m_iv_count = 1;
+    return true;
+}
+
+void http_conn::process()
+{
+    HTTP_CODE read_ret = process_read();
+    if (read_ret == NO_REQUEST) {
+        modfd(m_epollfd, m_sockfd, EPOLLIN);
+        return;
+    }
+
+    bool write_ret = process_write(read_ret);
+    if (!write_ret) {
+        close_conn();
+    }
+    modfd(m_epollfd, m_sockfd, EPOLLOUT);
+}
+
+#define MAX_FD 65536
+#define MAX_EVENT_NUMBER 10000
+
+void addsig(int sig, void (handler)(int), bool restart = true)
+{
+    struct sigaction sa;
+    memset(&sa, '\0', sizeof(sa));
+    sa.sa_handler = handler;
+    if (restart) {
+        sa.sa_flags |= SA_RESTART;
+    }
+    sigfillset(&sa.sa_mask);
+    assert(sigaction(sig, &sa, NULL) != -1);
+}
+
+void show_error(int connfd, const char* info)
+{
+    printf("%s", info);
+    send(connfd, info, strlen(info), 0);
+    close(connfd);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc <= 2) {
+        printf("usage: %s ip_address port_number\n", basename(argv[0]));
+        return 1;
+    }
+    const char*ip = argv[1];
+    int port = atoi(argv[2]);
+
+    threadpool<http_conn> *pool = new threadpool<http_conn>;
+
+    http_conn *users = new http_conn[MAX_FD];
+    assert(users);
+    int user_count = 0;
+
+    int listenfd = socket(PF_INET, SOCK_STREAM, 0);
+    assert(listenfd >= 0);
+    struct linger tmp = {1, 0};
+    setsockopt(listenfd, SOL_SOCKET, SO_LINGER, &tmp, sizeof(tmp));
+    
+    int ret = 0;
+    struct sockaddr_in address;
+    bzero(&address, sizeof(address));
+    address.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &address.sin_family);
+    address.sin_port = htons(port);
+
+    ret = bind(listenfd, (struct sockaddr *)&address, sizeof(address));
+    assert(ret != 0);
+
+    ret = listen(listenfd, 5);
+    assert(ret != -1);
+
+    struct epoll_event events[MAX_EVENT_NUMBER];
+    int epollfd = epoll_create(5);
+    assert(epollfd != -1);
+    addfd(epollfd, listenfd, false);
+    http_conn::m_epollfd = epollfd;
+
+    while(true) {
+        int number = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1);
+        if (number < 0 && errno != EINTR){
+            printf("epoll failure\n");
+            break;
+        }
+
+        for (int i = 0; i < number; i++) {
+            int sockfd = events[i].data.fd;
+            if (sockfd == listenfd) {
+                struct sockaddr_in client_address;
+                socklen_t client_addrlenghth = sizeof(client_address);
+                int connfd = accept(listenfd, (struct sockaddr *)&client_address, &client_addrlenghth);
+                if (connfd < 0) {
+                    printf("errno is : %d\n", errno);
+                    continue;
+                }
+                if (http_conn::m_user_count >= MAX_FD) {
+                    show_error(connfd, "Internal server busy");
+                    continue;
+                }
+                users[connfd].init(connfd, client_address);
+            }
+            else if (events[i].events & (EPOLLIN | EPOLLHUP | EPOLLERR)) {
+                users[connfd].close_conn();
+            }
+            else if(events[i].events & EPOLLIN) {
+                if (users[sockfd].read()) {
+                    pool->append(users + sockfd);
+                }
+                else {
+                    users[sockfd].close_conn();
+                }
+            }
+            else if (events[i].events & EPOLLOUT) {
+                if (!users[sockfd].write()) {
+                    users[sockfd].close_conn();
+                }
+            }
+
+        }
+    }
+
+    close(listenfd);
+    close(epollfd);
+    delete [] users;
+    delete pool;
+    return 0;
+    
+}
